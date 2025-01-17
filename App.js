04677d6301f4530d4ea068b9bec2d3a4c37b1f21
Satender Rathore <satenderjpr@gmail.com>
2025-01-17 18:20:28 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * <div id='parent'>
+ *  <div id='child1'>
+ *      <h1>First</h1>
+ *      <h2>Second</h2>
+ *  </div>
+ *  <div id='child2'>
+ *      <h3>Third</h1>
+ *      <h4>Fourth</h2>
+ *  </div>
+ * <div>
+ * 
+ * 
+ */
+
+
+
+const element = React.createElement('div', {id: 'parent'}, [
+    React.createElement("div", {id: "child1"}, [
+        React.createElement('h1', {}, 'First'),
+        React.createElement('h2', {}, 'Second')
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement('h3', {}, 'Third'),
+        React.createElement('h4', {}, 'Fourth')
+    ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(element);
